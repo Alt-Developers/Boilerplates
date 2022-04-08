5734bcfd28409a622c6d2e991edabc66ca336045
Jirat Chutrakul <jiratiesstudios@gmail.com>
2022-04-09 02:41:49 +0700

    Boilerplates | React Typescript Template 9/4/2022

--- React Typescript/src/App.tsx
+++ React Typescript/src/App.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import Starter from "./components/Starter";
+import { useDispatch } from "react-redux";
+import { modalActions } from "./context/modalSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
   return (
     <>
       <Starter />
--- React Typescript/src/components/Starter.tsx
+++ React Typescript/src/components/Starter.tsx
@@ -4,21 +4,23 @@ const Starter: React.FC<{}> = props => {
       <section
         className="starter"
         style={{
-          padding: "2rem",
+          padding: "3rem",
           height: "fit-content",
           backgroundColor: "#5c5cf2",
           color: "#fff",
           position: "relative",
         }}>
         <h1>Welcome to a new React Typescript Project</h1>
-        <p>Things included: axios, redux-toolkit, framer motion</p>
+        <p>
+          Things included: axios, redux-toolkit, framer motion, simple modal
+        </p>
         <i
           className="bx bxl-react"
           style={{
             position: "absolute",
             top: "22%",
             right: "2rem ",
-            fontSize: "4rem",
+            fontSize: "6rem",
             animation: "rotation 8s infinite linear",
           }}></i>
       </section>
